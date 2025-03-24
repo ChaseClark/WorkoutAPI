75de40302e1996ebc67a86e3504e522993d5ad7d
Chase Clark <chase_clark07@hotmail.com>
2025-03-24 15:01:23 -0400

    formatting

--- Models/Exercise.cs
+++ Models/Exercise.cs
@@ -3,7 +3,7 @@ public class Exercise
 {
     public int Id { get; set; }
     public required string Name { get; set; }
-    public int CategoryID {get; set;}
+    public int CategoryID { get; set; }
     public Category Category { get; set; } = null!;
     public required string UserId { get; set; }
     public required ApplicationUser User { get; set; }
